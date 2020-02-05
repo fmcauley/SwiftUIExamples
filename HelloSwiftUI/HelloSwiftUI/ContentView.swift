8ed51067c95f19dbeccbabe5ea11c71686811269
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-05 15:54:21 -0500

    FM - inital commit for Swift UI project

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by MLS Discovery on 2/5/20.
+//  Copyright © 2020 TeamAmerica. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
