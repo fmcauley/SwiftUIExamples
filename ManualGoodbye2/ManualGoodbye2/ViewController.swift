b6514c4b2a9ed9cad067fd3cc4ff4993906321b8
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-05 15:21:48 -0500

    FM - third pass at working out the UI and linking for this sample project

---
+++ ManualGoodbye2/ManualGoodbye2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManualGoodbye2
+//
+//  Created by MLS Discovery on 2/5/20.
+//  Copyright © 2020 TeamAmerica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
