676c66f2a54ed1db0b12ba69bd1e2f69424c47a4
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-05 15:09:34 -0500

    FM - made the app work without any help from the UI

--- ManualGoodBye1/ManualGoodBye1/ViewController.swift
+++ ManualGoodBye1/ManualGoodBye1/ViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let greetingLabel = UILabel() //moved here so that the action for the button can see the label
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let greetingLabel = UILabel()
+        
         greetingLabel.textColor = .secondaryLabel
         greetingLabel.font = UIFont.preferredFont(forTextStyle: .title1)
         greetingLabel.text = "Hello"
+        
+        let greetingButton = UIButton(type: .roundedRect)
+        greetingButton.setTitle("Press Here", for: .normal)
+        greetingButton.addTarget(self, action: #selector(greet), for: .touchUpInside)
         
         let stackView = UIStackView()
         view.addSubview(stackView)
@@ -29,8 +35,12 @@ class ViewController: UIViewController {
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         stackView.addArrangedSubview(greetingLabel)
-        
-        
+        stackView.addArrangedSubview(greetingButton)
+    }
+    
+    @objc private func greet(_ button: UIButton) {
+        button.isEnabled = false
+        greetingLabel.text = "Hello, World!"
     }
 
 
